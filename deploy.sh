4c503d0962ee3220d001bafd29a09179420df4eb
Moky <mokyue@163.com>
2015-01-19 13:55:16 +0800

    Source updated: Mon Jan 19 13:55:16     2015

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
+git config --global user.name "Moky"
+git config --global user.email "mokyue@163.com"
 rm -rf /E/Workspace/Github/hexo_src_for_moky.cc/.deploy
 hexo deploy
--- git_update.sh
+++ git_update.sh
@@ -1,3 +1,5 @@
+git config --global user.name "Moky"
+git config --global user.email "mokyue@163.com"
 git add .
 git commit -m "Source updated: $(date)"
 git push origin master
