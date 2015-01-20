558e105a173f4ea5099e87d38ba9c77a13234a18
Moky <mokyue@163.com>
2015-01-20 17:27:41 +0800

    Source updated: Tue Jan 20 17:27:41     2015

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 git config --global user.name "Moky"
 git config --global user.email "mokyue@163.com"
+git config --global core.autocrlf false
 rm -rf /E/Workspace/Github/hexo_src_for_moky.cc/.deploy
 hexo deploy
