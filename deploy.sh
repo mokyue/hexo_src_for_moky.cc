b27d96a11dea58df8096739ef5dc6cbd9df15fc0
Moky <mokyue@163.com>
2015-02-03 15:16:27 +0800

    clean source

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-git config --global user.name "Moky"
-git config --global user.email "mokyue@163.com"
-git config --global core.autocrlf false
-rm -rf /E/Workspace/Github/hexo_src_for_moky.cc/.deploy
-hexo deploy
--- generate.sh
+++
@@ -1,7 +0,0 @@
-hexo clean
-rm -rf /E/Workspace/Github/hexo_src_for_moky.cc/public/*
-hexo generate
-cp /E/Workspace/Github/hexo_src_for_moky.cc/CNAME /E/Workspace/Github/hexo_src_for_moky.cc/public
-cp /E/Workspace/Github/hexo_src_for_moky.cc/robots.txt /E/Workspace/Github/hexo_src_for_moky.cc/public
-cp /E/Workspace/Github/hexo_src_for_moky.cc/LICENSE /E/Workspace/Github/hexo_src_for_moky.cc/public
-cp /E/Workspace/Github/hexo_src_for_moky.cc/README.md /E/Workspace/Github/hexo_src_for_moky.cc/public
