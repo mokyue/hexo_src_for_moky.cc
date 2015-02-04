2a825ad7b8105427c0445674a4321ffbb0915c3f
Moky <mokyue@163.com>
2015-02-04 17:30:23 +0800

    Source updated: Wed Feb  4 17:30:24     2015

--- generate.sh
+++ generate.sh
@@ -1,9 +1,10 @@
 hexo clean
-rm -rf $(cd `dirname $0` && pwd)/public/*
+BASE_DIR=$(cd `dirname $0` && pwd)
+rm -rf ${BASE_DIR}/public/*
 hexo generate
-cp $(cd `dirname $0` && pwd)/CNAME $(cd `dirname $0` && pwd)/public
-cp $(cd `dirname $0` && pwd)/robots.txt $(cd `dirname $0` && pwd)/public
-cp $(cd `dirname $0` && pwd)/LICENSE $(cd `dirname $0` && pwd)/public
-cp $(cd `dirname $0` && pwd)/README.md $(cd `dirname $0` && pwd)/public
-cp $(cd `dirname $0` && pwd)/googlea7f3bc2968c9cd14.html $(cd `dirname $0` && pwd)/public
-cp $(cd `dirname $0` && pwd)/baidu_verify_hWI4mkl2O3.html $(cd `dirname $0` && pwd)/public
+cp ${BASE_DIR}/CNAME ${BASE_DIR}/public
+cp ${BASE_DIR}/robots.txt ${BASE_DIR}/public
+cp ${BASE_DIR}/LICENSE ${BASE_DIR}/public
+cp ${BASE_DIR}/README.md ${BASE_DIR}/public
+cp ${BASE_DIR}/googlea7f3bc2968c9cd14.html ${BASE_DIR}/public
+cp ${BASE_DIR}/baidu_verify_hWI4mkl2O3.html ${BASE_DIR}/public
