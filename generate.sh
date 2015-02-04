66c4b54790d51e603a02861105bbeda33b2e4be5
Moky <mokyue@163.com>
2015-02-04 09:59:05 +0800

    Source updated: Wed Feb  4 09:59:06     2015

--- generate.sh
+++ generate.sh
@@ -5,3 +5,5 @@ cp $(cd `dirname $0` && pwd)/CNAME $(cd `dirname $0` && pwd)/public
 cp $(cd `dirname $0` && pwd)/robots.txt $(cd `dirname $0` && pwd)/public
 cp $(cd `dirname $0` && pwd)/LICENSE $(cd `dirname $0` && pwd)/public
 cp $(cd `dirname $0` && pwd)/README.md $(cd `dirname $0` && pwd)/public
+cp $(cd `dirname $0` && pwd)/googlea7f3bc2968c9cd14.html $(cd `dirname $0` && pwd)/public
+cp $(cd `dirname $0` && pwd)/baidu_verify_hWI4mkl2O3.html $(cd `dirname $0` && pwd)/public
