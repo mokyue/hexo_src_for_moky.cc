16415e52ae10f60ade3035ed47efa8478152939d
Moky <mokyue@163.com>
2015-02-12 20:15:12 +0800

    Source updated: Thu Feb 12 20:15:13     2015

--- generate.sh
+++ generate.sh
@@ -8,3 +8,4 @@ cp ${BASE_DIR}/LICENSE ${BASE_DIR}/public
 cp ${BASE_DIR}/README.md ${BASE_DIR}/public
 cp ${BASE_DIR}/googlea7f3bc2968c9cd14.html ${BASE_DIR}/public
 cp ${BASE_DIR}/baidu_verify_hWI4mkl2O3.html ${BASE_DIR}/public
+cp ${BASE_DIR}/baidu-verify-FADDB8C099.txt ${BASE_DIR}/public
