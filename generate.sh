8e3b2919e6dbde1fe2408d0a03298bfee14bbe2a
Moky <mokyue@163.com>
2015-02-13 10:14:44 +0800

    Source updated: Fri Feb 13 10:14:45     2015

---
+++ generate.sh
@@ -0,0 +1,5 @@
+hexo clean
+BASE_DIR=$(cd `dirname $0` && pwd)
+rm -rf ${BASE_DIR}/public/*
+hexo generate
+cp ${BASE_DIR}/root/* ${BASE_DIR}/public
---
+++ github_sync.sh
@@ -0,0 +1,5 @@
+git config --global user.name "Moky"
+git config --global user.email "mokyue@163.com"
+git add -A
+git commit -m "Source updated: $(date)"
+git push origin master
